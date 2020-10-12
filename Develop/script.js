591d0d51c9b3a244d3733c3a3e25d2cc09f37b85
j9210 <71297542+j9210@users.noreply.github.com>
2020-10-11 19:32:04 -0500

    wrote generatePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,55 @@
-// Assignment code here
+// start generatePassword function
+var generatePassword = function () {
+
+  var length = prompt("How long do you want your password? Choose between 8 and 128 characters.");
+
+  if (length < 8 || length > 128 || length === null || isNaN(length)) {
+    length = prompt("Your password must be between 8 and 128 characters. Please try again.");
+    return generatePassword();
+  }
+
+  // confirm character types 
+  var lowerCase = confirm("Would you like lower case letters in your password?");
+  var upperCase = confirm("Would you like upper case letters in your password?")
+  var numChar = confirm("Would you like numbers in your password?");
+  var specialChar = confirm("Would you like special characters in your password?");
+
+  // define character types through empty charset string
+  var charset = "";
+
+  if (lowerCase) {
+    charset += "abcdefghijklmnopqrstuvwxyz";
+  }
+
+  if (upperCase) {
+    charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+
+  if (numChar) {
+    charset += "123456789";
+  }
+
+  if (specialChar) {
+    charset += " !#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+    charset += '"';
+  }
+
+  if (charset === "") {
+    window.alert("Sorry, you need more character types in your password.");
+    generatePassword();
+  }
+
+  var randomPassword = "";
+
+  // for loop that will generate random password
+  for (var i = 0; i < length; i++) {
+    randomPassword += charset.charAt(Math.floor(Math.random() * charset.length));
+  }
+
+  return randomPassword;
+
+};
+
 
 
 // Get references to the #generate element
